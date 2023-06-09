a3060b356d2001772bbf9a79675fb0311ab4b648
Tri <vutriminh@gmail.com>
2023-06-09 17:41:15 -0400

    create-react-app with button test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders a button that generates a random card', () => {
+    const { getByTestId } = render(<App />)
+    const nameElement = getByTestId('random-button')
+
+    expect(nameElement).toBeInTheDocument()
+})
