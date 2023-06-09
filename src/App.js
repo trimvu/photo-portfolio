967f7617b5fa0557a33f5b8df371fa736072c119
Tri <vutriminh@gmail.com>
2023-06-09 19:02:13 -0400

    added base layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  
   const [count, setCount] = useState(0)
+
   return (
     <div>
       <button onClick={() => setCount((count) => count + 1)} data-testid="random-button">
